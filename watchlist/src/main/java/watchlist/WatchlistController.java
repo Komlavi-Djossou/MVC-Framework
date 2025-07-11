92d1b8deb741ffc4c3f8c3a1dc823d7bf350aeea
Komlavi-Djossou <kdaffairs24@gmail.com>
2025-07-11 10:52:29 -0600

    Initial commit: set up Spring Boot Watchlist project

---
+++ watchlist/src/main/java/watchlist/WatchlistController.java
@@ -0,0 +1,50 @@
+package watchlist;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class WatchlistController {
+    List<WatchlistItem> watchlistItems = new ArrayList<WatchlistItem>();
+    private static int index = 1;
+
+    @GetMapping("/watchlistItemForm")
+    public ModelAndView showWatchlistItemForm() {
+        String viewName = "watchlistItemForm";
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("watchlistItem", new WatchlistItem());
+        return new ModelAndView (viewName, model);
+    }
+
+    @PostMapping("/watchlistItemForm")
+    public ModelAndView submitWatchlistItemForm (WatchlistItem watchlistItem){
+
+        watchlistItem.setId(index++);
+        watchlistItems.add(watchlistItem);
+
+        RedirectView redirect = new RedirectView();
+        redirect.setUrl("/watchlist");
+
+        return new ModelAndView(redirect);
+
+    }
+
+    @GetMapping("/watchlist")
+    public ModelAndView getWatchlist () {;
+
+        String viewName = "watchlist";
+        Map<String,Object> model = new HashMap<String,Object>();
+        model.put("watchlistItems",watchlistItems);
+        model.put("numberOfMovies",watchlistItems.size());
+
+        return new ModelAndView(viewName, model);
+    }
+}
