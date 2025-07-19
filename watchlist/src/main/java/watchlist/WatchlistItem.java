9e191b31992f46c6663e0cb6f92849685cf78919
Komlavi-Djossou <kdaffairs24@gmail.com>
2025-07-19 14:28:19 -0600

    Add validation to WatchlistItem fields and enable form error handling
    - Added @NotBlank to title and @Size(max=50) to comment in WatchlistItem.java
    - Updated submitWatchlistItemForm() method to use @Valid and BindingResult
    - Modified watchlistItemForm.html to display validation error messages for title and comment using Thymeleaf

--- watchlist/src/main/java/watchlist/WatchlistItem.java
+++ watchlist/src/main/java/watchlist/WatchlistItem.java
@@ -1,11 +1,18 @@
 package watchlist;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 
 public class WatchlistItem {
 
         private Integer id;
+
+        @NotBlank(message = "Please enter the title")
         private String title;
         private String rating;
         private String priority;
+
+        @Size(max=50,  message="Comment should be maximum 50 characters")
         private String comment;
 
        
---
+++ watchlist/src/test/java/watchlist/WatchlistControllerTest.java
@@ -0,0 +1,42 @@
+package watchlist;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@WebMvcTest(WatchlistController.class)
+@RunWith(SpringRunner.class)
+public class WatchlistControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void shouldDisplayWatchlistItemForm() throws Exception {
+        mockMvc.perform(get("/watchlistItemForm"))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(view().name("watchlistItemForm"))
+                .andExpect(model().size(1))
+                .andExpect(model().attributeExists("watchlistItem"));
+    }
+
+    @Test
+    public void shouldSubmitWatchlistItemForm() throws Exception {
+        mockMvc.perform(post("/watchlistItemForm")
+                        .param("title", "Inception")
+                        .param("rating", "9")
+                        .param("priority", "1")
+                        .param("comment", "Must watch again"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/watchlist"));
+    }
+
+
+}
